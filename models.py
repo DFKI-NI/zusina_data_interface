45f183d6156496b1151635f031617d4013aa83a9
andredfki <143592695+andredfki@users.noreply.github.com>
2023-09-11 10:42:37 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
+
 class Product(models.Model):
-    brand =  models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
     product_description = models.CharField(max_length=200)
     is_manufacturer = models.BooleanField(default=False)
     productid = models.CharField(max_length=100)
     brandid = models.CharField(max_length=100)
     ecolabel = models.CharField(max_length=100)
-    #ecolabel = models.Choices()
     ecolabel_informations = models.CharField(max_length=500)
     startdate = models.DateField(null=True)
     enddate = models.DateField(null=True)
