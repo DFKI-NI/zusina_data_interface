3fa9bca095ae2ab85bd6fe8532c238f55c603a2d
andredfki <143592695+andredfki@users.noreply.github.com>
2023-09-11 10:46:33 +0200

    Update serializer.py

--- serializer.py
+++ serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Product
 
+
 class ProductSerializer(serializers.ModelSerializer):
-     class Meta:
+    class Meta:
         model = Product
-        fields = ["brand", "product_description", "is_manufacturer", "productid", "brandid", "ecolabel", "ecolabel_informations", "startdate", "enddate"]
+        fields = ["brand", "product_description", "is_manufacturer", "productid", "brandid", 
+                  "ecolabel", "ecolabel_informations", "startdate", "enddate"]
